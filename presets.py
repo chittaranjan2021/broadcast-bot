14e9708bb656e24c7dbb4dc844a540425a540e02
chittaranjan2021 <83960157+chittaranjan2021@users.noreply.github.com>
2021-10-15 20:26:07 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,9 +1,9 @@
 class Presets(object):
     WELCOME_MESSAGE = """
 <b>Hello.. </b>{}
-<code>I'm an official telecaster bot
+<code>Movies channel @netflix_hindi_hd
 maintained by</code> <b>{}</b>
-<code>Keep me unblocked to receive ma official updates.</code>
+<code>join @allbotdeveloper for official updates.</code>
     """
     USERS_LIST = "<b>Total:</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
     WAIT_MSG = "<b>Please Wait...</b>"
